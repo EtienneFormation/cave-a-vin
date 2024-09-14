d5709728ed89324666342a07fa602916fa9baea0
excilyano <agahnim13@gmail.com>
2024-09-15 00:26:48 +0200

    correction M02_TP01

---
+++ src/main/java/fr/eni/cave/bo/Client.java
@@ -0,0 +1,36 @@
+package fr.eni.cave.bo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter
+@ToString(of = {"pseudo", "nom", "prenom"})
+@EqualsAndHashCode(of = "pseudo")
+@Entity
+@Table(name = "CAVE_CLIENT")
+@Builder
+public class Client {
+	@Id
+	@Column(name = "LOGIN", nullable = false, length = 255)
+	private String pseudo;
+	
+	@Column(name = "PASSWORD", nullable = false, length = 68)
+	private String password;
+	
+	@Column(name = "LAST_NAME", nullable = false, length = 98)
+	private String nom;
+	
+	@Column(name = "FIRST_NAME", nullable = false, length = 150)
+	private String prenom;
+	
+}
