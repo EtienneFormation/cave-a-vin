ff0af773cfc92bd301430613d8cd02a2e916a152
excilyano <agahnim13@gmail.com>
2024-09-15 23:07:09 +0200

    correction M02_TP05

--- src/main/java/fr/eni/cave/bo/Client.java
+++ src/main/java/fr/eni/cave/bo/Client.java
@@ -1,41 +1,23 @@
 package fr.eni.cave.bo;
 
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter
-@ToString(of = {"pseudo", "nom", "prenom"})
-@EqualsAndHashCode(of = "pseudo")
+@SuperBuilder
+
 @Entity
 @Table(name = "CAV_CLIENT")
-@Builder
-public class Client {
-	@Id
-	@Column(name = "LOGIN", nullable = false, length = 255)
-	private String pseudo;
-	
-	@Column(name = "PASSWORD", nullable = false, length = 68)
-	private String password;
-	
-	@Column(name = "LAST_NAME", nullable = false, length = 98)
-	private String nom;
-	
-	@Column(name = "FIRST_NAME", nullable = false, length = 150)
-	private String prenom;
-	
+public class Client extends Utilisateur {
 	@OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
 	private Adresse adresse;
 }
---
+++ src/main/java/fr/eni/cave/bo/Utilisateur.java
@@ -0,0 +1,41 @@
+package fr.eni.cave.bo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = { "pseudo" })
+@ToString(of = { "pseudo", "nom", "prenom" })
+@SuperBuilder
+
+@Entity
+@Table(name = "CAV_USER")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Utilisateur {
+	@Id
+	@Column(name = "LOGIN", nullable = false, length = 255)
+	private String pseudo;
+	
+	@Column(name = "PASSWPORD", nullable = false, length = 68)
+	private String password;
+	
+	@Column(name = "LAST_NAME", nullable = false, length = 90)
+	private String nom;
+	
+	@Column(name = "FIRST_NAME", nullable = false, length = 150)
+	private String prenom;
+}
