cf070e1071b696b1db85789fe25dadfc65a7dbb8
Etienne CASSIN <ecassin@campus-eni.fr>
2024-09-18 13:24:47 +0200

    correction pathvariable

--- src/main/java/fr/eni/cave/controllers/BouteilleController.java
+++ src/main/java/fr/eni/cave/controllers/BouteilleController.java
@@ -2,19 +2,20 @@ package fr.eni.cave.controllers;
 
 import fr.eni.cave.bo.Bouteille;
 import fr.eni.cave.services.BouteilleService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/caveavin/bouteilles")
 public class BouteilleController {
-    @Autowired
-    private BouteilleService bouteilleService;
+    private final BouteilleService bouteilleService;
+
+    public BouteilleController(BouteilleService bouteilleService) {
+        this.bouteilleService = bouteilleService;
+    }
 
     @GetMapping
     public ResponseEntity<?> getAllBouteilles() {
@@ -22,6 +23,57 @@ public class BouteilleController {
         if (bouteilles == null || bouteilles.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
+
+        return ResponseEntity.ok(bouteilles);
+    }
+
+    // @RequestMapping
+
+    @GetMapping("/{id}")
+    // @RequestMapping(path = "/{id}", method = RequestMethod.GET) // alternative avec RequestMapping
+    public ResponseEntity<?> getBouteilleById(@PathVariable("id") String id) {
+        int idx;
+        try {
+            idx = Integer.parseInt(id);
+        } catch (NumberFormatException e) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Format d'identifiant incorrect");
+        }
+
+        Bouteille bouteille = bouteilleService.chargerBouteilleParId(idx);
+        if (bouteille == null)
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(bouteille);
+    }
+
+    @GetMapping("/region/{id}")
+    public ResponseEntity<?> getBouteilleByRegionId(@PathVariable("id") String id) {
+        int idx;
+        try {
+            idx = Integer.parseInt(id);
+        } catch (NumberFormatException e) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Format d'identifiant incorrect");
+        }
+
+        List<Bouteille> bouteilles = bouteilleService.chargerBouteillesParRegion(idx);
+        if (bouteilles == null || bouteilles.isEmpty())
+            return ResponseEntity.noContent().build();
+
+        return ResponseEntity.ok(bouteilles);
+    }
+
+    @GetMapping("/couleur/{id}")
+    public ResponseEntity<?> getBouteilleByCouleurId(@PathVariable("id") String id) {
+        int idx;
+        try {
+            idx = Integer.parseInt(id);
+        } catch (NumberFormatException e) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Format d'identifiant incorrect");
+        }
+
+        List<Bouteille> bouteilles = bouteilleService.chargerBouteillesParCouleur(idx);
+        if (bouteilles == null || bouteilles.isEmpty())
+            return ResponseEntity.noContent().build();
 
         return ResponseEntity.ok(bouteilles);
     }
